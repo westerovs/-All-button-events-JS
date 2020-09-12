406c0a552868ce678f7982491cad453d6e3ab299
westerovs <westerovs@mail.ru>
2020-09-12 18:25:06 +0300

    начало записи

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
-const buttons = document.querySelectorAll('.button')
+/*eslint-disable*/
+const buttons = document.querySelectorAll('.button');
 const wrapper = document.querySelector('.wrapper');
 const btnJustClick = document.querySelector('.button--click')
 const btnToggle = document.querySelector('.button--toggle')
 const btnFlag = document.querySelector('.button--flag')
 const btnUpDown = document.querySelector('.button--upDown')
+const btnLeft = document.querySelector('.button--left');
+const btnRight = document.querySelector('.button--right');
+
+function changeImage(first, second) {
+    wrapper.style.backgroundImage = `url(img/${first}.jpg)`
+    wrapper.style.pointerEvents = 'none';
+
+    setTimeout(() => {
+        wrapper.style.backgroundImage = `url(img/${second}.jpg)`
+        wrapper.style.pointerEvents = 'auto';
+    }, 1000);
+}
 
 function onSignaling() {
     const signal = new Audio('./signal.mp3')
     signal.play()
-
-    wrapper.style.backgroundImage = 'url(../img/1.jpg)'
-    wrapper.style.pointerEvents = 'none';
-
-    setTimeout(() => {
-        wrapper.style.backgroundImage = 'url(../img/0.jpg)'
-        wrapper.style.pointerEvents = 'auto';
-    }, 1000);
+    changeImage(1, 0)
 }
 
 // =========================== CLICK
@@ -27,9 +33,9 @@ btnJustClick.addEventListener('click', () => {
 btnToggle.addEventListener('click', function () {
     btnToggle.classList.toggle('button--press')
 
-    wrapper.style.backgroundImage = 'url(../img/3.jpg)'
+    wrapper.style.backgroundImage = 'url(img/3.jpg)'
     if (!btnToggle.classList.contains('button--press')) {
-        wrapper.style.backgroundImage = 'url(../img/0.jpg)'
+        wrapper.style.backgroundImage = 'url(img/0.jpg)'
     }
 })
 
@@ -40,24 +46,24 @@ btnFlag.addEventListener('click', function () {
     flag = !flag
 
     if (flag) {
-        wrapper.style.backgroundImage = 'url(../img/3.jpg)'
+        wrapper.style.backgroundImage = 'url(img/3.jpg)'
         btnFlag.classList.add('button--press')
     }
     else {
         btnFlag.classList.remove('button--press')
-        wrapper.style.backgroundImage = 'url(../img/0.jpg)'
+        wrapper.style.backgroundImage = 'url(img/0.jpg)'
     }
 })
 
 // =========================== up / down
 
 btnUpDown.addEventListener('mousedown', () => {
-    wrapper.style.backgroundImage = 'url(../img/4.jpg)'
+    wrapper.style.backgroundImage = 'url(img/4.jpg)'
     btnUpDown.innerHTML = 'UP'
 })
 
 btnUpDown.addEventListener('mouseup', () => {
-    wrapper.style.backgroundImage = 'url(../img/0.jpg)'
+    wrapper.style.backgroundImage = 'url(img/0.jpg)'
     btnUpDown.innerHTML = 'DOWN'
 })
 
